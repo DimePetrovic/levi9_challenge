a10b6cc79016a0a68467a2655dae153fbb6353e4
JovanMihic99 <95135165+JovanMihic99@users.noreply.github.com>
2024-11-23 19:17:13 +0100

    Revert "Revert "Revert "Mysql database"""

--- nodeApp/controllers/matches.js
+++ nodeApp/controllers/matches.js
@@ -1,14 +1,10 @@
 const MatchesService = require('../services/matches');
-const axios = require("axios")
+
 // POST /matches
 function createMatch(req, res) {
     try {
         MatchesService.createMatch(req.body);
-        const postData = {a:"a"};
-        axios.post('https://lfxbmuwrntckwbozc22crpndoa0lpdbn.lambda-url.eu-central-1.on.aws/', postData)
-    
-        res.status(200).send()
- 
+        res.status(200).send();
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
