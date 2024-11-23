71f2f23ae8793cc18fefe86bd361971069ec5a74
JovanMihic99 <jovanmihic99@gmail.com>
2024-11-23 19:07:27 +0100

    adsdas

--- nodeApp/controllers/matches.js
+++ nodeApp/controllers/matches.js
@@ -8,7 +8,8 @@ function createMatch(req, res) {
         axios.post('https://lfxbmuwrntckwbozc22crpndoa0lpdbn.lambda-url.eu-central-1.on.aws/', postData)
     
         res.status(200).send()
-
+ 
+        
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
